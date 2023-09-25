4c750d05ff8f23be5d624ae60dbae098a7207765
mostaafaa <mostafaalaa2017@gmail.com>
2023-09-25 23:39:49 +0200

    z

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -16,7 +16,7 @@ const Testimonial = () => {
                 height={80}
                 alt="dp"
               />
-              <h2 className="text-gray-500 font-black text-[20px]">Elsa Doe</h2>
+              <h2 className="text-gray-500 font-black text-[20px]">MA designes</h2>
               <p>CEO & Founder Invision</p>
               <Image
                 className="inline-block py-2"
@@ -26,8 +26,9 @@ const Testimonial = () => {
                 alt="quotes"
               />
               <p className="max-w-[200px] text-gray-500">
-                Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor dolor
-                sit amet.
+                WE HAVE THE 
+                MOST AWESOME 
+                  DESIGNES
               </p>
             </div>
           </div>
@@ -35,13 +36,14 @@ const Testimonial = () => {
           <div className="bg-red-600 bg-[url(/cta-banner.jpg)] bg-cover h-[500px] rounded-2xl grid place-items-center">
             <div className="bg-[#ffffffab] min-w-[270px] sm:min-w-[300px] md:min-w-[500px] rounded-xl py-8 sm:px-8 px-2 grid place-items-center gap-3">
               <button className="bg-blackish text-white p-2 rounded-md">
-                25% DISCOUNT
+                10% DISCOUNT ON 
+                 ALL PRODUCTS
               </button>
               <h2 className="font-extrabold text-2xl text-[#272727]">
-                Summer Collection
+                LAUNCHING GIFTS
               </h2>
               <p className="text-gray-500 text-[20px]">
-                Starting @ $20 <b>Shop Now</b>
+                CLAIM THE PROMO CODE: MA10 AND <b>Shop Now</b>
               </p>
             </div>
           </div>
